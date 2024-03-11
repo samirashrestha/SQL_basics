b5c8932eb10127aebe13ccec78351afa4af174f6
Samira Shrestha <samira.shresthaa@gmail.com>
2024-03-10 22:35:36 -0500

    SQL file imported

---
+++ student_information.sql
@@ -0,0 +1,8 @@
+Insert Into new_students (student_id, last_name, first_name, address, city, email)
+Values ( '1', 'Shrestha', 'Samira', '0006 Oceanside', 'Euless', 'samira.shrestha@gmail.com'),
+('2', 'Sophia', 'Leo', '0009 Mc Author', 'Irving', 'samira.shrestha@gmail.com'),
+('3', 'Lovlina', 'Simmons', '0005 Las Colinas', 'Irving', 'samira.shrestha@gmail.com'),
+('4', 'Sammy', 'Sanna', '0006 Haslet', 'Forthworth', 'samira.shrestha@gmail.com'),
+('5', 'Tina', 'Tippy', '0006 Saginaw', 'Forthworth', 'samira.shrestha@gmail.com');
+
+
